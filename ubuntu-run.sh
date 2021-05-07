79263a74449b18719640dbad2b07ac11bfa8ec98
yash <73551736+yashlearnpython@users.noreply.github.com>
2021-05-07 12:07:49 +0530

    Update ubuntu-run.sh

--- ubuntu-run.sh
+++ ubuntu-run.sh
@@ -1,1 +1,3 @@
 echo this can run commands in terminal tho
+sudo -E apt-get -qq update
+sudo -E apt-get -qq install bc build-essential zip curl libstdc++6 git wget python gcc clang libssl-dev rsync flex bison ccache openjdk-8-jdk expect aria2
